aa8f3c119b17c8d59cba152b7ea01d2a04c8ab37
ashish0807 <ashishshrma.0807@gmail.com>
2024-09-09 00:31:18 +0530

    frontend-part

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,6 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Navbar from "@/components/navbar"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata = {
   title: "Create Next App",
@@ -20,10 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <Navbar/>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
